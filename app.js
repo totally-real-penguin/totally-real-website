22f247589b052ef9c3d300f835a8e2c1979b1ca9
(Totally Real) Penguin <totallyrealpenguin@gmail.com>
2024-05-11 23:20:01 +0100

    basic website layout

---
+++ app.js
@@ -0,0 +1,19 @@
+const themeButton = document.getElementById("theme");
+const body = document.body;
+
+const storedTheme = window.localStorage.getItem("theme");
+
+if (storedTheme) {
+    body.classList.add(storedTheme)
+}else {
+    body.classList.add("light")
+}
+themeButton.onclick = () => {
+    if (body.classList.contains("light")) {
+        body.classList.replace("light","dark")
+        localStorage.setItem("theme","dark")
+    }else {
+        body.classList.replace("dark","light")
+        localStorage.setItem("theme","light")
+    }
+}
