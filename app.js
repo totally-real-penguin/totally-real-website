f015c0ae398aa786a8db68b6e9e612e7ec002188
(Totally Real) Penguin <totallyrealpenguin@gmail.com>
2024-05-11 23:24:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const themeButton = document.getElementById("theme");
 const body = document.body;
 
-const storedTheme = window.CacheStorage.getItem("theme");
+const storedTheme = CacheStorage.getItem("theme");
 
 if (storedTheme) {
     body.classList.add(storedTheme)
@@ -11,9 +11,9 @@ if (storedTheme) {
 themeButton.onclick = () => {
     if (body.classList.contains("light")) {
         body.classList.replace("light","dark")
-        localStorage.setItem("theme","dark")
+        CacheStorage.setItem("theme","dark")
     }else {
         body.classList.replace("dark","light")
-        localStorage.setItem("theme","light")
+        CacheStorage.setItem("theme","light")
     }
 }
