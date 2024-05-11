2e66f852e87040147d6552057767cf500ed72499
(Totally Real) Penguin <totallyrealpenguin@gmail.com>
2024-05-11 23:57:49 +0100

    cookie clicker

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const themeButton = document.getElementById("theme");
 const body = document.body;
 
-const storedTheme = CacheStorage.getItem("theme");
+document.cookie = "theme=light";
 
-if (storedTheme) {
-    body.classList.add(storedTheme)
-}else {
-    body.classList.add("light")
+function getCookie(name) {
+    const cDecoded = decodeURIComponent(document.cookie)
+    const cArray = cDecoded.split(";");
+    let result  = null
+    cArray.forEach(e => {
+        if(e.indexOf(name) == 0){
+            result = e.substring(name.length + 1)
+        }
+    })
+    return result
 }
+
+if (getCookie("theme")) {
+    body.classList.add(getCookie("theme"))
+}
+
 themeButton.onclick = () => {
     if (body.classList.contains("light")) {
         body.classList.replace("light","dark")
-        CacheStorage.setItem("theme","dark")
+        document.cookie = "theme=dark"
     }else {
         body.classList.replace("dark","light")
         CacheStorage.setItem("theme","light")
+        document.cookie = "theme=light"
     }
 }
